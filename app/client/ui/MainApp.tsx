3ec7a1557a6d5832be3511e29bb0b1363527e7ad
Gergely Ujvari <ujvari@nolmecolindor.com>
2016-12-05 15:54:14 +0100

    Moving files around

--- app/client/ui/MainApp.tsx
+++ app/client/ui/MainApp.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { DemoMap } from "./components/DemoMap";
+import { DemoMap } from "./google-map-react/DemoMap";
 
 
 export const MainApp:React.StatelessComponent<{}> = () => {
